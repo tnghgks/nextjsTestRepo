0f58e309c4afcd1b11f90ad132370dd408084270
tnghgks <tnghgks@naver.com>
2023-02-27 23:23:26 +0900

    design: Global CSS 추가 및 Normalize

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import { Layout } from "@/components/Layout";
+import CssBaseline from "@mui/material/CssBaseline";
 import type { AppProps } from "next/app";
+import "@/styles/global.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Layout>
+        <CssBaseline />
         <Component {...pageProps} />
       </Layout>
     </>
