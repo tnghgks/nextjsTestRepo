ba33faf2cade0c3e7cb01868ecbfac82ce81a3dd
tnghgks <tnghgks@naver.com>
2023-03-02 23:50:22 +0900

    design: 컴포넌트 ui 수정

--- src/components/MainBanner.tsx
+++ src/components/MainBanner.tsx
@@ -1,3 +1,12 @@
+import styled from "@emotion/styled";
+
 export default function MainBanner() {
-  return <div>MainBanner</div>;
+  return <Container>MainBanner</Container>;
 }
+
+const Container = styled.section`
+  width: 100%;
+  height: 250px;
+  border: 3px solid #000000;
+  overflow: hidden;
+`;
--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -1,3 +1,12 @@
+import styled from "@emotion/styled";
+
 export default function ProjectList() {
-  return <div>ProjectList</div>;
+  return <Container>ProjectList</Container>;
 }
+
+const Container = styled.section`
+  width: 100%;
+  height: 250px;
+  border: 3px solid #000000;
+  overflow: hidden;
+`;
