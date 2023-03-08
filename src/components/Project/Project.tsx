be0de487be1b285583c79aa0905538871204f401
HOSU HAN <tnghgks@naver.com>
2023-03-09 04:49:53 +0900

    feat: project list UI 및 기능구현 (#3)
    * feat: ProjectSchema 구현
    
    * remove: MainBanner 컴포넌트 삭제
    
    * feat: Project 컴포넌트 생성
    
    * feat: User Props Interface 생성

---
+++ src/components/Project/Project.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+export default function Project({ project }: any) {
+  return (
+    <Card sx={{ maxWidth: 300 }}>
+      <CardMedia sx={{ height: 140 }} image="" title={project.title} />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {project.title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {project.description}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+}
--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -1,10 +1,24 @@
 import styled from "@emotion/styled";
+import Project from "./Project/Project";
 
 export default function ProjectList() {
-  return <Container>ProjectList</Container>;
+  return (
+    <Container>
+      <ProjectContainer>
+        <Project></Project>
+      </ProjectContainer>
+    </Container>
+  );
 }
 
 const Container = styled.section`
+  width: 100%;
+  height: 250px;
+  border: 3px solid #000000;
+  overflow: hidden;
+`;
+
+const ProjectContainer = styled.section`
   width: 100%;
   height: 250px;
   border: 3px solid #000000;
